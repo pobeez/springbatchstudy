b7413cc0546f4e48d5dc7b9efce503b5ba8866fe
pobeez <pobeez@gmail.com>
2017-03-23 18:15:10 +0900

    gradle java plugin

---
+++ dailylog/2017/studylog_2017.md
@@ -0,0 +1,16 @@
+## Log directory for 2017
+
+* 2017/03/20
+    * 개발 툴 설치 (installTools.md)
+* 2017/03/21
+    * N/A
+* 2017/03/22
+    * N/A
+* 2017/03/23
+    * `Gradle 철저 입문` 교제 학습
+        * 3장: 그레이들 시작 (完)
+            * /springbatch/gradle/study/ch3/JAVA_PROJECT
+            * /springbatch/gradle/study/ch3/WEB_PROJECT
+        * 4장: 자바 프로젝트 빌드
+            * 4.3.1. 기본적인 빌드정의와 실행(~ p201)
+                *  /springbatch/gradle/study/ch4/example_project
---
+++ springbatch/gradle/study/ch4/example_project/src/main/java/com/example/util/BasicArithmaticOperations.java
@@ -0,0 +1,60 @@
+package com.example.util;
+
+import java.math.BigDecimal;
+
+
+/***
+*/
+public class BasicArithmaticOperations {
+    /***
+    */
+    static public BigDecimal plus (String a, String b) {
+        return plus(new BigDecimal(a), new BigDecimal(b));
+    }
+
+    /**/
+    static public BigDecimal plus(BigDecimal a, BigDecimal b) {
+        return a.add(b);
+    }
+    
+    static public BigDecimal minus(String a, String b) {
+        return minus(new BigDecimal(a), new BigDecimal(b));
+    }
+
+    static public BigDecimal minus(BigDecimal a, BigDecimal b) {
+        return a.subtract(b);
+    }
+
+    static public BigDecimal multiply(String a, String b) {
+        return multiply(new BigDecimal(a), new BigDecimal(b));
+    }
+
+    static public BigDecimal multiply(BigDecimal a, BigDecimal b) {
+        return a.multiply(b);
+    }
+    
+    static public BigDecimal divide(String a, String b) {
+        return divide(new BigDecimal(a), new BigDecimal(b));
+    }
+
+    static public BigDecimal divide(BigDecimal a, BigDecimal b) {
+        return a.divide(b);
+    }    
+}
+
+/*D:\source\git\springbatchstudy\springbatch\gradle\study\ch4\example_project>gradle build
+:compileJava
+:processResources NO-SOURCE
+:classes
+:jar
+:assemble
+:compileTestJava NO-SOURCE
+:processTestResources NO-SOURCE
+:testClasses UP-TO-DATE
+:test NO-SOURCE
+:check UP-TO-DATE
+:build
+
+BUILD SUCCESSFUL
+
+Total time: 6.331 secs*/
